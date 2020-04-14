88a16272b8119a0bfab9f30e4291803dc7f13383
Jeremy Xue <gg831006@gmail.com>
2020-04-14 13:55:59 +0800

    🎉 (project): Initial commit

---
+++ Swift-Dice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-Dice
+//
+//  Created by Jeremy Xue on 2020/4/14.
+//  Copyright © 2020 Jeremy Xue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
